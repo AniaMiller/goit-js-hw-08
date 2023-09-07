087931e679cfd8eb8c5de03a43d9cfbf7d470203
Ania Miller <it.zawodowo@gmail.com>
2023-09-07 23:47:43 +0200

    Removed some redundant code and fix for 0 length

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,19 +9,7 @@ const updateTime = function(data) {
     localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
 };
 
-const savedTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+const savedTime = JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0;
 
 player.on('timeupdate', throttle(updateTime, 1000));
-player.setCurrentTime(savedTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(savedTime)
